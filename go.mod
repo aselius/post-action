4faf3dfca6408b1a44483c651c34c7f2930b63c4
Jaewoo Park <jaewoopark91@gmail.com>
2021-05-13 14:30:21 -0700

    init add

---
+++ go.mod
@@ -0,0 +1,11 @@
+module postaction
+
+go 1.14
+
+require (
+	github.com/jinzhu/gorm v1.9.16
+	github.com/stretchr/objx v0.3.0 // indirect
+	github.com/stretchr/testify v1.7.0
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	gorm.io/gorm v1.21.9 // indirect
+)
---
+++ server_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	_ "net/http"
+	_ "net/http/httptest"
+	"testing"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+
+	_"github.com/stretchr/testify/assert"
+)
+
+func setupTestDB() (*gorm.DB) {
+	var dbConnInfo = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+	"localhost", 5432, "postgres", "postgres", "postgres")
+
+	gormDB, err := gorm.Open("postgres", dbConnInfo)
+	if err != nil {
+		panic(err)
+	}
+	
+	return gormDB
+}
+
+var (
+	dB = setupTestDB()
+)
+
+func TestMain(m *testing.M) {
+	err := dB.Exec("create table if not exists foobar (id serial primary key, name varchar(128) not null, points integer)").Error
+	if err != nil {
+		panic(err)
+	}
+
+	os.Exit(m.Run())
+}
